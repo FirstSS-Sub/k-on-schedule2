8a77e6dc1ce6fe952e2bbfb6d7e069f3b74b32ff
FirstSS-Sub <sota.monokuma-bass@docomo.ne.jp>
2020-07-12 08:27:13 +0900

    About.vue にAPIとの通信例を追加

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="onamae">お名前ボタン</button>
+    <ul>
+      <li v-for="(data, key) in datalist" v-bind:key="key">
+        {{data.last}} {{data.first}}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      datalist: []
+    }
+  },
+  methods: {
+    onamae: function () {
+      this.$axios
+        .get('http://localhost:5000/create_user')
+        .then(res => {
+          this.datalist = res.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
