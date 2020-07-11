0ad3b10b20f485ce9456b857528533655be6af60
FirstSS-Sub <sota.monokuma-bass@docomo.ne.jp>
2020-07-12 08:27:16 +0900

    About.vue にAPIとの通信例を追加

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios' //追記
 
+Axios.defaults.baseURL = 'http://localhost:5000'
+
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 
--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -3,6 +3,7 @@
     <h1>This is an about page</h1>
     <button @click="onamae">お名前ボタン</button>
     <ul>
+      <!-- v-for は使っても使わなくてもkeyを設定しないとダメみたい -->
       <li v-for="(data, key) in datalist" v-bind:key="key">
         {{data.last}} {{data.first}}
       </li>
@@ -21,7 +22,7 @@ export default {
   methods: {
     onamae: function () {
       this.$axios
-        .get('http://localhost:5000/create_user')
+        .get('/create_user')
         .then(res => {
           this.datalist = res.data
         })
