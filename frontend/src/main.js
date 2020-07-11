c5f0d5d0026e110f7d7da792f7b3427fd2967d49
FirstSS-Sub <sota.monokuma-bass@docomo.ne.jp>
2020-07-12 08:28:39 +0900

    About.vue にAPIとの通信例を追加

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from 'axios' //追記
+import axios from 'axios' // 追記
 
-Axios.defaults.baseURL = 'http://localhost:5000'
+axios.defaults.baseURL = 'http://localhost:5000'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
